a84a18ee44078139916e7ef7c22e627650911994
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-10-11 20:14:57 +0600

    questions design complete

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -11,7 +11,9 @@ const Quiz = () => {
             <h1 className="text-2xl text-slate-800 my-4"> {name} Quiz</h1>
             <h1 className="text-md text-slate-600"> total -{total}</h1>
             <div className="container mx-auto">
-
+                {
+                    questions.map(Singlequestion => <QuestionCard Singlequestion={Singlequestion}></QuestionCard>)
+                }
             </div>
         </div>
     );
