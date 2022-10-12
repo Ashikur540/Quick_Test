1ada41ecbe75948dc7f50e5255e9df54353bf941
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-10-12 18:19:57 +0600

    anser toast modified

--- src/Components/QuestionCard/QuestionCard.jsx
+++ src/Components/QuestionCard/QuestionCard.jsx
@@ -51,29 +51,18 @@ const QuestionCard = ({ Singlequestion }) => {
 
     // show answer
     const showAnswer = (ans) => {
-        // toast.info(ans, {
-        //     position: "top-center",
-        //     autoClose: 5000,
-        //     hideProgressBar: false,
-        //     closeOnClick: true,
-        //     pauseOnHover: true,
-        //     draggable: true,
-        //     progress: undefined,
-        //     theme: "light",
-        // });
-        return <div>
-            {/* <!-- The button to open modal --> */}
-            {/* <label htmlFor="my-modal-4" className="btn modal-button">open modal</label> */}
+        let correctAns = `Answer: ${ans}`
+        toast.info(<p className="text-md font-bold"><>{correctAns}</></p>, {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
 
-            {/* <!-- Put this part before </body> tag-- > */}
-            <input type="checkbox" id="my-modal-4" className="modal-toggle" />
-            <label htmlFor="my-modal-4" className="modal cursor-pointer">
-                <label className="modal-box relative" for="">
-                    <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                </label>
-            </label>
-        </div >
     }
 
     return (
